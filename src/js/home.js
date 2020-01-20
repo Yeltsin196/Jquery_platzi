c8df96ca453c9684211bd99906e3d84a9ab0f186
Yeltsin Ramirez <yeltsin.ramirez@emqu.net>
2020-01-20 14:08:28 -0400

    2do Promesas

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,41 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUserAll = new Promise(function(res,err){
+  setTimeout(function(){
+    //luego de 3 segundos
+   res('todo bien');
+  },5000);
+ 
+});
+
+const getUser = new Promise(function(res,err){
+  setTimeout(function(){
+    //luego de 3 segundos
+   res('todo bien');
+  },3000);
+ 
+});
+
+/* getUser.then(function(){
+  console.log('todo bien');
+}).catch( function(mensaje){
+  console.log(mensaje);
+}); */
+
+Promise.all([
+  getUser,
+  getUserAll 
+]).then(function(message){
+  console.log(message);
+}).catch(function(message){
+  console.log(message)
+});
+
+/* $.ajax('https://randomuser.me/api/',{
+  method:'GET',
+  sucess: function(data){
+    console.log(data)
+  }
+}); */
